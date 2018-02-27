9842c60fad3dc4680b560bae08bbec54c784b8d4
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-02-27 14:44:49 +0700

    improvement icon

--- resources/views/stack/css.blade.php
+++ resources/views/stack/css.blade.php
@@ -9,6 +9,7 @@
 
   @push('global-styles')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="icon" href="drive.png">
   @endpush
 
 @endif
