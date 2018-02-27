81ceec407d8366de736ee18dfc053cdc598d7f74
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-02-27 17:46:11 +0700

    improve mobile size display

--- resources/views/stack/css.blade.php
+++ resources/views/stack/css.blade.php
@@ -5,11 +5,19 @@
     <link rel="icon" href="img/favicon.png">
   @endpush
 
+  @push('custom-css')
+    <link rel="stylesheet" href="css/styles.css"></script>
+  @endpush
+
 @else
 
   @push('global-styles')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="icon" href="img/favicon.png">
+  @endpush
+
+  @push('custom-css')
+    <link rel="stylesheet" href="css/styles.css"></script>
   @endpush
 
 @endif
