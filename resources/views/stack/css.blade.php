81929d647e6b07d119362deb5ea2b67cdc13f93e
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2018-02-27 18:42:40 +0700

    fix icon

--- resources/views/stack/css.blade.php
+++ resources/views/stack/css.blade.php
@@ -2,7 +2,7 @@
   
   @push('global-styles')
     <link rel="stylesheet" href="vendor/bootstrap/v4/css/bootstrap.min.css"></script>
-    <link rel="icon" href="img/favicon.png">
+    <link rel="icon" href="drive.png">
   @endpush
 
   @push('custom-css')
@@ -13,14 +13,11 @@
 
   @push('global-styles')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<<<<<<< HEAD
     <link rel="icon" href="drive.png">
-=======
   @endpush
 
   @push('custom-css')
     <link rel="stylesheet" href="css/styles.css"></script>
->>>>>>> heroku-dev
   @endpush
 
 @endif
